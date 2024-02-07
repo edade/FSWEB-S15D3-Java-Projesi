94a1ac76bbdccbbafeca707032c45d8bd1673290
Eda Kalaycioglu <edakalaycioglu@gmail.com>
2024-02-07 15:28:59 +0300

    Company needs your help done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,42 @@
+import model.Employee;
+
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Employee> employeeList = new LinkedList<>();
+        Employee e1 = new Employee(1,"Eda", "Kalayci");
+        Employee e2 = new Employee(1,"Eda", "Kalayci");
+        Employee e3 = new Employee(2,"Yavuz", "Irmak");
+        Employee e4 = new Employee(2,"Yavuz", "Irmak");
+
+        employeeList.add(e1);
+        employeeList.add(e2);
+        employeeList.add(e3);
+        employeeList.add(e4);
+
+        Map<Integer, Employee> employeeMap = new HashMap<>();
+        List<Employee> removedEmployees = new ArrayList<>();
+
+        Iterator<Employee> iterator = employeeList.iterator();
+        while(iterator.hasNext()){
+            Employee employee = iterator.next();
+            if(employee == null){
+                System.out.println("null data");
+                continue;
+            }
+            if(employeeMap.containsKey(employee.getId())){
+                removedEmployees.add(employee);
+                //iterator.remove();
+            }
+            else{
+                employeeMap.put(employee.getId(), employee);
+            }
+        }
+        System.out.println(employeeList);
+        System.out.println(employeeMap);
+        System.out.println(removedEmployees);
+
+
     }
 }
